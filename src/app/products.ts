04d6e9a359aa57e0991f5352ede7e2f0b01d8236
Galymbekov <galymbekov.dastan@gmail.com>
2021-02-23 13:51:50 +0600

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,69 @@
+export const products = [
+  {
+    name: 'Amazon Basics RJ45 Cat-6 Ethernet Patch Internet Cable - 10 Feet (3 Meters)',
+    rating: '4.7 out of 5',
+    description: 'Cat-6 Ethernet patch cable for wired home and office networks',
+    image: 'https://m.media-amazon.com/images/I/61DQtJPAJ1L._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'Amazon Basics Nylon Braided Lightning to USB Cable',
+    rating: '4.7 out of 5',
+    description: 'Incredible Charging Speed: Wide-diameter wires and reduced cord resistance maximize charging speed (supports up to 2.4 amps charging current).',
+    image: 'https://m.media-amazon.com/images/I/61stA5Z9W0L._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'iPhone Charger, YUNSONG 3Pack 6FT Nylon Braided Lightning Cable',
+    rating: '4.7 out of 5',
+    description: 'DURABLE DESIGN: Covered with braided-nylon jacket. Polished aluminum alloy housing not only pairs perfectly with your iPhone iPad, but also protects against physical stresses.',
+    image: 'https://m.media-amazon.com/images/I/71Xu5kRT4XL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'Amazon Basics CL2-Rated Coaxial TV Antenna Cable - 4 feet',
+    rating: '4.5 out of 5',
+    description: '75 OHM RG6 coaxial A/V cable with 2 nickel-plated F-type male connectors; safe for use indoors or out; UL-rated for in-wall use',
+    image: 'https://m.media-amazon.com/images/I/81gQOyY4xgL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'Printer Cable 10 FT/ 3 Meter, NIMASO USB 2.0 Printer Cable',
+    rating: '4.8 out of 5',
+    description: 'Pratical Printer Cable: NIMASO USB 2.0 Printer cable can be widely used for your scanner, printer, server ,hard drive,piano and other usb b devices to a laptop,computer(Mac/PC) or other USB-enabled devices.Just Plug and Play,no drive required.',
+    image: 'https://m.media-amazon.com/images/I/61tpm2opQiL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'Forney 70452 Wire Rope, Vinyl Coated Aircraft Cable',
+    rating: '4.8 out of 5',
+    description: '250 feet vinyl coated galvanized wire rope aircraft cable',
+    image: 'https://m.media-amazon.com/images/I/81qztE8qFUL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'Micro USB Cable (5-Pack, 6FT) Android Charger',
+    rating: '4.6 out of 5',
+    description: 'USB 2.0 Cable - A Male to Micro B; Supports 480-Mbps transmission speed',
+    image: 'https://m.media-amazon.com/images/I/71ee9YgbO3L._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'Apple Lightning to USB Cable (2 m)',
+    rating: '4.7 out of 5',
+    description: 'This USB 2.0 cable connects your iPhone, iPad, Mac, AirPods or iPod with Lightning Connector to your computers USB port for syncing and charging.',
+    image: 'https://m.media-amazon.com/images/I/41ePOd0vlOL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'VCE 5-Pack Coaxial Cable Connector, F-Type Coax RG6 Cable Extension Adapter Gold Plated',
+    rating: '4.7 out of 5',
+    description: 'Coax coupler with gold plating resists corrosion and provides rigidity',
+    image: 'https://m.media-amazon.com/images/I/612bLNwxvOL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+   {
+    name: 'Anker iPhone Charger Cable 10 Foot, Powerline+ II Lightning Cable',
+    rating: '4.7 out of 5',
+    description: 'Staying Power: Lasts 30x longer than ordinary cables-proven in a laboratory environment to withstand 30000 bends.',
+    image: 'https://m.media-amazon.com/images/I/71oTBLwSqpL._AC_UY327_FMwebp_QL65_.jpg'
+  },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
